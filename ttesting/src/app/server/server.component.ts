20220a0b11c2c763417d139975c658b0fe541f87
Simer0996 <singhsimer09@gmail.com>
2023-07-18 22:57:40 -0700

    learning ngclass

--- ttesting/src/app/server/server.component.ts
+++ ttesting/src/app/server/server.component.ts
@@ -4,12 +4,18 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
-    styleUrls: ['./server.component.css']
+    styleUrls: ['./server.component.css'],
+    styles: [`
+    .online {
+        color:white;
+    }
+    `]
 })
 
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = "Offline"
+
 
     constructor() {
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
